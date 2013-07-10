a08899644fd3063168c39fd610087315a9fa3e82
Edgar Gomes de Araujo <talktoedgar@gmail.com>
2013-07-10 17:57:07 +0200

    clean-up

--- src/Hammer/VoxBox/VoxConnFinder.hs
+++ src/Hammer/VoxBox/VoxConnFinder.hs
@@ -6,14 +6,14 @@
 {-# OPTIONS_GHC -fno-warn-unused-do-bind #-}
 
 module Hammer.VoxBox.VoxConnFinder
-  ( grainFinder
-  , finderVoxConn
-  , VoxConn    (voxConnRange, voxConnMap, voxConnList)
-  , HasConn    (..)
-  , HasVoxConn (..)
-  , GridConn   (..)
-  , Cont       (..)
-  ) where
+       ( grainFinder
+       , finderVoxConn
+       , VoxConn    (voxConnRange, voxConnMap, voxConnList)
+       , HasConn    (..)
+       , HasVoxConn (..)
+       , GridConn   (..)
+       , Cont       (..)
+       ) where
 
 import qualified Data.HashMap.Strict            as HM
 import qualified Data.Vector                    as V
@@ -34,7 +34,8 @@ import           Hammer.VoxBox.Base
 
 -- ================================================================================
   
--- | Function to find grains in voxels with @Int@ property. Based on @finderVoxConn@ for Vector Int.
+-- | Function to find grains in voxels with 'Int' property. Based on 'finderVoxConn' for
+-- 'Vector' 'Int'.
 grainFinder :: VoxBox Int -> Maybe (VoxBox GrainID, HashMap Int (Vector VoxelPos))
 grainFinder vb@VoxBox{..} = let
   found = finderVoxConn grainID dimension
@@ -44,17 +45,19 @@ grainFinder vb@VoxBox{..} = let
       in return (vb { grainID = new_vec }, l)
     _ -> Nothing
 
--- | Main function from this module. Given a certain property within a data container (typically
--- @Vector@ or @HashMap@) and together with its @VoxBoxRange@, this function scans and group the
--- connected neighbor voxels by given them a unique ID.
--- The scan is based on a divide & conquer algorithm where the connected voxels are tested along the
--- boarder of the divided space. The algorithm can run in a multi-core device for extra speed.
-finderVoxConn :: (HasConn a, Cont ic a, HasVoxConn c g, NFData (c Int), NFData g, GridConn g)
-              => ic a -> VoxBoxRange -> VoxConn c g
+-- | Main function from this module. Given a certain property within a data container
+-- (typically 'Vector' or 'HashMap') and together with its 'VoxBoxRange', this function
+-- scans and group the connected neighbor voxels by given them a unique ID. The scan is
+-- based on a divide & conquer algorithm where the connected voxels are tested along the
+-- boarder of the divided space. The algorithm can run in a multi-core device for extra
+-- speed.
+finderVoxConn :: ( HasConn a, Cont ic a, HasVoxConn c g
+                 , NFData (c Int), NFData g, GridConn g
+                 )=> ic a -> VoxBoxRange -> VoxConn c g
 finderVoxConn vec range = force $ go range
   where
     merge = mergeVoxConn vec range
-    
+
     --go :: VoxBoxRange -> Maybe (VoxConn c a)
     go boxrange = case splitInTwoBox boxrange of
       Nothing -> let
@@ -72,22 +75,18 @@ finderVoxConn vec range = force $ go range
           return (merge b1 b2)
 
 
-tryConn :: (HasConn a, Cont c a, GridConn g)
-        => c a
-        -> VoxBoxRange
-        -> CartesianDir
-        -> g
-        -> Vector (g, g)
+tryConn :: (HasConn a, Cont c a, GridConn g
+           )=> c a -> VoxBoxRange -> CartesianDir -> g -> Vector (g, g)
 tryConn as vbr dir p = case getConnPos dir p of
-  Just connPair 
+  Just connPair
     | testConn connPair -> connPair `V.cons` filterCross
     | otherwise         -> filterCross
   _ -> filterCross
   where
-    {-# INLINE getter #-}    
+    {-# INLINE getter #-}
     getter = getValue as vbr . toVoxelPos
     getter' = getValue as vbr . toVoxelPos
-  
+
     testConn (a,b) = isConn (getter a) (getter b) dir
 
     filterCross = let
@@ -109,13 +108,13 @@ commitConn ::(HasVoxConn c g, GridConn g)
            -> (g, g)
            -> (c Int, HashMap Int (Vector g))
 commitConn range maps@(connMap, connList) (p1, p2) = let
-  
+
   compareBoth gid1 gid2
     -- Check if it has been megered before (Int_1 == Int_2)
     | gid1 == gid2 = Nothing
     | gid1 <  gid2 = takeToFrom gid1 gid2
     | otherwise    = takeToFrom gid2 gid1
-      
+
   -- Take the VID set from gid1 to gid2
   -- gid1 -> Dominate grain ID
   -- gid2 -> its content will be merge into gid1
@@ -127,7 +126,7 @@ commitConn range maps@(connMap, connList) (p1, p2) = let
       gidSet     = HM.lookup gid2 connList
       delGLM     = HM.delete gid2 connList
       modGLM set = HM.insertWith (V.++) gid1 set delGLM
-      
+
   commit = do
     ogid1 <- getValue connMap range (toVoxelPos p1)
     ogid2 <- getValue connMap range (toVoxelPos p2)
@@ -135,27 +134,27 @@ commitConn range maps@(connMap, connList) (p1, p2) = let
 
   in maybe maps id commit
 
-mergeDataList :: VoxConn c g -> VoxConn c g -> Maybe (HashMap Int (Vector g)) 
+mergeDataList :: VoxConn c g -> VoxConn c g -> Maybe (HashMap Int (Vector g))
 mergeDataList d1 d2 = let
   func x1 x2 = return $ HM.union x1 x2
   in mergeMaybe func (voxConnList d1) (voxConnList d2)
-     
-mergeVoxConn :: (HasConn a, Cont ic a, GridConn g, HasVoxConn c g)
-             => ic a -> VoxBoxRange -> VoxConn c g -> VoxConn c g -> VoxConn c g
+
+mergeVoxConn :: (HasConn a, Cont ic a, GridConn g, HasVoxConn c g
+                )=> ic a -> VoxBoxRange -> VoxConn c g -> VoxConn c g -> VoxConn c g
 mergeVoxConn as vbr d1 d2 = case mergeVoxBoxRange (voxConnRange d1) (voxConnRange d2) of
   Just (newBR, dir, wallbox) -> let
     wall         = scanWall dir wallbox
-    uniGrainMap  = mergeDataMap  d1 d2 
+    uniGrainMap  = mergeDataMap  d1 d2
     uniGrainList = mergeDataList d1 d2
-    wallGrid     = V.map toGridType wall 
+    wallGrid     = V.map toGridType wall
     withConn     = V.concatMap (tryConn as vbr dir) wallGrid
     newBD        = VoxConn newBR uniGrainMap uniGrainList
     in mixMaps newBD withConn
   _ -> error "[VoxConnFinder] I can't merge these two VoxBoxRange's"
 
--- | Extract a list of all @VoxelPos@ of the lower side of a plane
--- defined by the @CartesianDir@. Used to scan the connections along the
--- wall (boarder between two divided spaces).
+-- | Extract a list of all 'VoxelPos' of the lower side of a plane defined by the
+-- 'CartesianDir'. Used to scan the connections along the wall (boarder between two
+-- divided spaces).
 scanWall :: CartesianDir -> WallBoxRange -> Vector VoxelPos
 scanWall dir (WallBoxRange br) = let
   (lx, ux, ly, uy, lz, uz) = getBoxLinRange br
