b237eda530feed772328200f82f85f6da069a57a
Edgar Gomes <talktoedgar@gmail.com>
2013-09-28 01:54:04 +0200

    Fix bug #2 "Wrong alignment on getListSegs" where some of the sorted segement wasn't aligned. The bug was fixed and the module was partially refactored with function, data and class type simplification and cleanup. Some functions were renamed and the API has changed.

--- src/Hammer/Math/SortSeq.hs
+++ src/Hammer/Math/SortSeq.hs
@@ -5,133 +5,164 @@
 {-# LANGUAGE FlexibleContexts     #-}
 {-# LANGUAGE UndecidableInstances #-}
 
-{- | Module to sort and classify sequence of segments e.g.
+{- | Module to sort and classify sequence of segments.
+For example:
 
-> closeSeq [(1,2), (2,5), (10, 1), (10,5)] = [(1,2), (2,5), (5, 10), (10, 1)]
+> sortSegs $ V.fromList [(1::Int,2::Int), (10,1), (2,5), (10,5)]
+> [LoopSeq fromList [(1,2),(2,5),(5,10),(10,1)]]
+
+> sortSegs $ V.fromList [(1::Int,11::Int), (10,1), (2,5), (10,5)]
+> [OpenSeq (1,11) fromList [(11,2),(2,5),(5,10),(10,1)]]
 
 -}
-
-
 module Hammer.Math.SortSeq
   ( SeqSeg  (..)
-  , SeqInv  (..)
   , SeqComp (..)
   , Seq (..)
-    -- * Vector based
-  , getVecSegs
-  , getVecSegsIndirect
-    -- * List based
-  , getListSegs
+    -- * Sorting sequences
+  , sortSegs
+  , sortSegsIndirect
+    -- * Filter sequence type
   , splitOpenLoop
+  , getOneOpen
+  , getOpens
   , getOneLoop
   , getLoops
-    -- * Old implamentation
-  , closeSeq
   ) where
 
 import           Control.Applicative ((<$>))
-import           Control.Monad       (liftM, zipWithM_)
 import           Control.Monad.ST    (runST)
-import           Data.Maybe          (isJust)
 import           Data.Vector         (Vector)
-  
+
 import qualified Data.Vector         as V
 import qualified Data.Vector.Mutable as VM
 import qualified Data.List           as L
 
 --import           Debug.Trace
 --dbg a = trace (">> " ++ show a) a
-        
--- ====================================== Data types ====================================
-        
-class (SeqComp (SeqUnit a))=> SeqSeg a where
-  type SeqUnit a
-  seqHead :: a -> SeqUnit a
-  seqTail :: a -> SeqUnit a
 
+-- ====================================== Data types ====================================
+
+-- | Basic class definition for sorting segments.
+class (SeqComp (SeqUnit a))=> SeqSeg a where
+  -- | What segments are made of.
+  type SeqUnit a
+  -- | Acquire the first element in the segment.
+  getSeqHead :: a -> SeqUnit a
+  -- | Acquire the last element in the segment.
+  getSeqTail :: a -> SeqUnit a
+  -- | How to invert the direction of the segment. The default implementation *doesn't*
+  -- invert the segment ('id').
+  seqInv :: a -> a
+  seqInv = id
+
+-- | Check the equality of two elements. If you need more complex equality test (e.g.
+-- within a tolerance error) implement by yourself otherwise a standard implementation is
+-- given with '==', therefore simple equality test can implemented by:
+--
+-- > instance SeqComp TheType
+--
 class (Eq a)=> SeqComp a where
   seqComp :: a -> a -> Bool
   seqComp = (==)
 
-class SeqInv a where
-  seqInv :: a -> a
-  seqInv = id
+-- | 'Seq' holds the results of the sorting algorithm by classifying it in open or closed
+-- sequences.
+data Seq a
+  = OpenSeq { openSeq :: Vector a }
+  | LoopSeq { loopSeq :: Vector a }
+
+-- ======================================= Instances =====================================
 
 instance (SeqComp a)=> SeqSeg (a, a) where
   type SeqUnit (a, a) = a
-  seqHead = fst
-  seqTail = snd
+  getSeqHead   = fst
+  getSeqTail   = snd
+  seqInv (a,b) = (b,a)
 
-instance SeqInv (a, a) where
-  seqInv (a,b) = (b,a) 
+instance (Show a, SeqSeg a)=> Show (Seq a) where
+  show (OpenSeq s) = "OpenSeq " ++ show s
+  show (LoopSeq s) = "LoopSeq " ++ show s
 
-instance SeqInv Int
+-- ====================================== Sort algorithm =================================
 
-instance (Show a, Show u, Show (c a), SeqSeg a)=> Show (Seq c a u) where
-  show (OpenSeq a b s) = "OpenSeq " ++ show (a,b) ++ " " ++ show s
-  show (LoopSeq s)     = "LoopSeq " ++ show s
-  
-data Seq c a u
-  = OpenSeq
-    { seqIni  :: !u --(SeqUnit a)
-    , seqEnd  :: !u --(SeqUnit a)
-    , seqList :: c a
-    }
-  | LoopSeq { loopList :: c a }
+-- | Sorts a vector of 'Int's used as reference (pointers) to another vector containing
+-- the values.
+sortSegsIndirect ::(SeqSeg b)=> Vector b -> Vector Int -> [Seq Int]
+sortSegsIndirect v = fst . fastSortSegs (getSeqHead . (v V.!)) (getSeqTail . (v V.!)) id
 
--- ====================================== Vector ====================================
+-- | Classify a list segments in 'OpenSeq' and 'LoopSeq', open and closed sequences. The
+-- resulting sequence is also aligned, it means that the tail of a segment match the head
+-- of the following segment and vice-verse. Branches and inter-loops generate have multiple
+-- solution and this function will return just one of them.
+sortSegs :: (SeqSeg a)=> Vector a -> [Seq a]
+sortSegs = fst . fastSortSegs getSeqHead getSeqTail seqInv
 
-getVecSegsIndirect ::(SeqSeg b)=> Vector b -> Vector Int -> [Seq Vector Int (SeqUnit b)]
-getVecSegsIndirect v = fst . sortSegs (seqHead . (v V.!)) (seqTail . (v V.!)) id
-
-getVecSegs :: (SeqSeg a, SeqInv a)=> Vector a -> [Seq Vector a (SeqUnit a)]
-getVecSegs = fst . sortSegs seqHead seqTail seqInv
-
-sortSegs :: (SeqComp b)=> (a -> b) -> (a -> b) -> (a -> a)
-         -> Vector a -> ([Seq Vector a b], Vector a)
-sortSegs getHead getTail inv es = runST $ do
+-- Internal function that actually does the sorting.
+fastSortSegs :: (SeqComp b)=> (a -> b) -> (a -> b) -> (a -> a)
+             -> Vector a -> ([Seq a], Vector a)
+fastSortSegs getHead getTail inv es = runST $ do
   v <- V.thaw es
   let
     maxsize = (V.length es) - 1
 
     mkSeq i xi f xf
-      | xi `seqComp` xf  = LoopSeq       <$> seg
-      | otherwise = OpenSeq xi xf <$> seg
+      | xi `seqComp` xf  = LoopSeq <$> seg
+      | otherwise        = OpenSeq <$> seg
       where
-        len = f - i + 1 
+        len = f - i + 1
         seg = V.unsafeFreeze $ VM.slice i len v
-    
+
     getFs i = do
       f <- VM.read v i
       return (getHead f, getTail f)
 
+    -- Revert the sequence between two given positions
     revert a b = let
-      (q, r) = (abs (b - a)) `quotRem` 2
-      n      = if r == 0 then q else q+1
-      forward  = take n [a ..]
-      backward = take n [b, b-1 ..]
-      in zipWithM_ (VM.swap v) forward backward 
-  
-    runSeq breaks start
-      | start > maxsize = return breaks
-      | otherwise = do
-        (ta, tb) <- getFs start
-        (endB, tagA) <- scanForward tb start (start + 1)
-        (endA, tagB) <- scanForward ta endB  (endB + 1)
-        if endA == endB
-          then do 
-          brk <- mkSeq start ta endB tagA
-          runSeq (brk:breaks) (endB + 1)
-          else do
-          brk <- mkSeq start tagB endA tagA
-          revert start endB
-          runSeq (brk:breaks) (endA + 1)
+      l        = min a b
+      u        = max a b
+      n        = u - l + 1
+      (q, _)   = n `quotRem` 2
+      forward  = V.enumFromN     l      q
+      backward = V.enumFromStepN u (-1) q
+      in do
+        V.zipWithM_ (VM.swap v) forward backward
+        V.mapM_ invert (V.enumFromN l n)
 
+    -- Invert the segment at given position
     invert i = do
       x <- VM.read v i
       VM.write v i (inv x)
-    
-    --scanForward Nothing ref _ = return (ref, Nothing)
+
+    -- Finds chunks of sequences
+    runSeq breaks start
+      | start > maxsize = return breaks
+      | otherwise = do
+        (ta, tb)     <- getFs start
+        -- There are two possible solution:
+        -- -|=========|++++++|
+        -- -|====|====|++++++|
+        -- First scan the sequence from tb
+        (endB, tagA) <- scanForward tb start (start + 1)
+        -- Then search for segments from ta
+        (endA, tagB) <- scanForward ta endB  (endB + 1)
+        -- if the start position is equal to the final position in the second search
+        if endA == endB
+           -- then nothing was found and the only sequence is in the first scan
+          then do
+          brk <- mkSeq start ta endB tagA
+          runSeq (brk:breaks) (endB + 1)
+          -- else revert the first scan to match to the second one
+          else do
+          revert start endB
+          brk <- mkSeq start tagA endA tagB
+          runSeq (brk:breaks) (endA + 1)
+
+    -- Build a sequence by looking for the segment containing a element equals 'target'
+    -- and swapping it with the segement in the next position.
+    -- > 'target' is the head or tail of a segment that is looking for a match
+    -- > 'ref' is the position that contains the 'target' element
+    -- > 'i' is the current position that ranges from 'ref+1' until the last position
     scanForward target ref i
       | i <= maxsize = do
         (a, b) <- getFs i
@@ -141,105 +172,44 @@ sortSegs getHead getTail inv es = runST $ do
         foo a b n
           | target `seqComp` a = VM.swap v n (ref+1) >> scanForward b (ref+1) (ref+2)
           | target `seqComp` b = VM.swap v n (ref+1) >> invert (ref+1) >> scanForward a (ref+1) (ref+2)
-          | n < maxsize = scanForward target ref (n+1)
-          | otherwise   = return (ref, target)
-  
+          | n < maxsize        = scanForward target ref (n+1)
+          | otherwise          = return (ref, target)
+
   breaks <- runSeq [] 0
   imv    <- V.unsafeFreeze v
   return (breaks, imv)
 
--- ====================================== List ====================================
+-- ==================================== Process results ==================================
 
-closeSeq :: (SeqSeg a, SeqInv a)=>[a] -> Maybe [a]
-closeSeq []        = Nothing
-closeSeq [_]       = Nothing
-closeSeq (seed:xs) = liftM (seed:) (getSeq (seqTail seed) xs) 
-  where
-    --nextRef :: (SeqSeg a, Eq (SeqUnit a))=> SeqUnit a -> a -> Maybe (SeqUnit a)
-    nextRef x t
-      | x == a && x /= b = Just (b, t)
-      | x /= a && x == b = Just (a, seqInv t)
-      | otherwise        = Nothing
-      where
-        a = seqHead t
-        b = seqTail t
-
-    --isIn :: (SeqSeg a, Eq (SeqUnit a))=> SeqUnit a -> a -> Bool
-    isIn x    = isJust . nextRef x
-    --isClosure :: (SeqSeg a, Eq (SeqUnit a))=> a -> Bool
-    isClosure = isIn (seqHead seed)
-
-    --getSeq :: (SeqSeg a, Eq (SeqUnit a))=> SeqUnit a -> [a] -> Maybe ([a])
-    getSeq _ [] = Nothing
-    getSeq ref rs = case getNext ref rs of
-      Just (newRef, x, list)
-        | null list -> if isClosure x
-                       then Just [x]
-                       else Nothing
-        | otherwise -> do
-          prox   <- getSeq newRef list
-          return (x:prox)
-      _             -> Nothing
-      
-    --getNext :: (SeqSeg a, Eq (SeqUnit a))=> SeqUnit a -> [a] -> Maybe (a, [a])
-    getNext ref ss = case break (isIn ref) ss of
-      ([], [])   -> Nothing
-      (_ , [])   -> Nothing
-      (as, b:bs) -> do
-        (next, rest) <- nextRef ref b
-        return (next, rest, as ++ bs)
-
--- | Classify a list segments in @OpenSeq@ and @LoopSeq@
--- Expect chunk of linear sequences. Branches and interconnections
--- might show unpredictable behavior.
-getListSegs :: (SeqInv a, SeqSeg a)=> [a] -> [Seq [] a (SeqUnit a)]
-getListSegs ps = let
-  sl = let
-    toSeq x
-      | a == b    = LoopSeq [x]
-      | otherwise = OpenSeq a b [x]
-      where
-        a = seqHead x
-        b = seqTail x
-    in map toSeq ps
-
-  isIn i1 f1 (OpenSeq i2 f2 _) = i1 == i2 || i1 == f2
-                              || f1 == i2 || f1 == f2
-  isIn _ _ _ = False
-
-  rev = reverse . map seqInv
-
-  merge (OpenSeq i1 f1 sl1) (OpenSeq i2 f2 sl2)
-    | i1 == f2 && i2 == f1 = LoopSeq (sl2 ++ sl1)
-    | i1 == i2 && f1 == f2 = LoopSeq (sl2 ++ sl1)
-    | i1 == f2 = OpenSeq i2 f1 (sl2       ++       sl1)
-    | i2 == f1 = OpenSeq i1 f2 (sl1       ++       sl2)
-    | i1 == i2 = OpenSeq f1 f2 ((rev sl1) ++       sl2)
-    | f1 == f2 = OpenSeq i1 i2 (sl1       ++ (rev sl2))
-  merge _ _ = error "[SeqSeg] Can't merge loops. It's a bug."
-  
-  func [] rs = rs
-  func (x:xs) rs = case x of
-    OpenSeq i f _ -> case break (isIn i f) xs of
-      ([],[])    -> func xs (x:rs)
-      (_, [])    -> func xs (x:rs)
-      (as, b:bs) -> func (as ++ (merge x b):bs) rs
-    LoopSeq _    -> func xs (x:rs) 
-  in func sl []
-
-splitOpenLoop :: [Seq c a u] -> ([Seq c a u], [Seq c a u])
+-- | Split the result in open sequences (first element) and closed loops (second element).
+splitOpenLoop :: [Seq a] -> ([Seq a], [Seq a])
 splitOpenLoop = let
-  isOpen (OpenSeq _ _ _) = True
-  isOpen _               = False              
+  isOpen (OpenSeq _) = True
+  isOpen _           = False
   in L.partition isOpen
 
-getLoops :: (SeqSeg a)=> [Seq c a (SeqUnit a)] -> [c a]
+-- | Get only open sequences.
+getOpens :: (SeqSeg a)=> [Seq a] -> [Vector a]
+getOpens = let
+  foo (OpenSeq x) = x
+  foo _           = error "[SeqSeg] Exepection LoopSeq. It must be a bug."
+  in map foo . fst . splitOpenLoop
+
+-- | Successfully returns if there is one and only one open sequence.
+getOneOpen :: (SeqSeg a)=> [Seq a] -> Maybe (Vector a)
+getOneOpen ss = case getLoops ss of
+  [s] -> return s
+  _   -> Nothing
+
+-- | Get only closed loops sequences.
+getLoops :: (SeqSeg a)=> [Seq a] -> [Vector a]
 getLoops = let
   foo (LoopSeq x) = x
   foo _           = error "[SeqSeg] Exepection LoopSeq. It must be a bug."
-  in map foo . snd . splitOpenLoop 
-    
-getOneLoop :: (SeqSeg a)=> [Seq c a (SeqUnit a)] -> Maybe (c a)
+  in map foo . snd . splitOpenLoop
+
+-- | Successfully returns if there is one and only one closed loop.
+getOneLoop :: (SeqSeg a)=> [Seq a] -> Maybe (Vector a)
 getOneLoop ss = case getLoops ss of
   [s] -> return s
   _   -> Nothing
