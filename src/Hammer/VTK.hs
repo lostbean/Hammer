c817b0be2b4b98a9f9d9df318c1803d5250abc35
Edgar Gomes Araujo <edgar@silk.co>
2016-05-24 12:17:17 +0200

    Move VTK tools to its own package.

--- src/Hammer/VTK.hs
+++ src/Hammer/VTK.hs
@@ -1,197 +1,213 @@
-{-# LANGUAGE ExistentialQuantification #-}
-{-# LANGUAGE FlexibleInstances         #-}
-{-# LANGUAGE GADTs                     #-}
-{-# LANGUAGE MultiParamTypeClasses     #-}
-{-# LANGUAGE NamedFieldPuns            #-}
-{-# LANGUAGE OverloadedStrings         #-}
-{-# LANGUAGE RecordWildCards           #-}
-{-# LANGUAGE TypeSynonymInstances      #-}
-{-# OPTIONS_GHC -fno-warn-orphans      #-}
+{-# LANGUAGE
+    RecordWildCards
+  , FlexibleInstances
+  , FlexibleContexts
+  , TypeFamilies
+  #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 
--- | This module renders VTK files in XML format with ASCII and binary data. VTK file
--- format is used to visualize scientific data in three (less usually in two) dimensions.
--- One of the more important viewers for VTK files is Paraview <http://www.paraview.org/>.
--- More information about the VTK format can be found here:
--- <http://www.vtk.org/VTK/img/file-formats.pdf>
 module Hammer.VTK
-  ( VTK (..)
-    -- * Write to file
-  , writeUniVTKfile
-  , writeMultiVTKfile
-    -- * Make VTK
-  , mkUGVTK
-  , mkRLGVTK
-  , mkSGVTK
-  , mkSPVTK
-    -- * Add attributes
-  , mkCellAttr
-  , mkPointAttr
-  , mkPointValueAttr
-  , addPointAttr
-  , addPointValueAttr
-  , addCellAttr
-    -- * Classes for new attributes
-  , CellType    (..)
-  , RenderCell  (..)
-  , RenderPoint (..)
-  , RenderElemVTK
-  , VTKAttrPointValue
-  , VTKAttrPoint
-  , VTKAttrCell
+  ( module Hammer.VTK
+  , module Data.VTK
   ) where
 
-import qualified Data.ByteString.Lazy             as BSL
-import qualified Data.IntMap                      as IM
-import qualified Data.List                        as L
-import qualified Data.Vector                      as V
-import qualified Data.Vector.Unboxed              as U
-import qualified Text.XML.Generator               as X
+import qualified Data.Vector           as V
+import qualified Data.Vector.Unboxed   as U
+import qualified Data.HashMap.Strict   as HM
 
-import           Data.IntMap                      (IntMap)
-import           Data.Text                        (pack)
-import           Data.Vector.Unboxed              (Vector)
+import           Data.Maybe            (mapMaybe)
+import           Data.Monoid           ((<>))
+import           Data.Vector.Unboxed   (Vector, Unbox)
+import           Data.VTK
 
-import           Hammer.VTK.Types
-import           Hammer.VTK.VTKXMLTemplate
+import           Hammer.MicroGraph
+import           Hammer.Math.Algebra
+import           Hammer.VoxBox
+
+--import           Debug.Trace
+--dbg a = trace ("@@@@@@@>> " ++ show a) a
 
 -- =======================================================================================
 
--- | Write an unitary piece VTK file to disk. Set 'True' for binary format.
-writeUniVTKfile :: (RenderElemVTK a) => FilePath -> Bool -> VTK a -> IO ()
-writeUniVTKfile name isBinary = BSL.writeFile name . X.xrender . renderVTKUni isBinary
+newtype VoxBoxExt a = VoxBoxExt (VoxBox a)
 
--- | Write an multi-piece piece VTK file to disk. Set 'True' for binary format.
-writeMultiVTKfile :: (RenderElemVTK a) => FilePath -> Bool -> V.Vector (VTK a) -> IO ()
-writeMultiVTKfile name isBinary = BSL.writeFile name . X.xrender . renderVTKMulti isBinary
+class (Unbox elem, Unbox (VTKElem elem))=> RenderVox elem where
+  type VTKElem elem :: *
+  renderVox :: VoxBoxExt a -> elem -> VTKElem elem
 
--- | Creates an Unstructured Grid dataset. Use:
---
--- > mkUGVTK "name" (Vec.fromList [Vec3 0 0 0, Vec3 0 1 0]) [(0,1)]
---
-mkUGVTK :: (RenderElemVTK p, RenderCell shape, FastFoldable cont shape)
-        => String -> Vector p -> cont shape -> [VTKAttrPointValue p] -> [VTKAttrCell p] -> VTK p
-mkUGVTK name points cells pointData cellData = let
-  nameTxt = pack name
-  dataset = mkUnstructGrid points cells
-  in VTK nameTxt dataset pointData [] cellData
+instance RenderVox FaceVoxelPos where
+  type VTKElem FaceVoxelPos = (Int, Int, Int, Int)
+  renderVox = renderFacePos
 
--- | Creates an Rectilinear Grid dataset. Use:
---
--- > mkRLGVTK "name" (Vec.fromList [0,1,2]) (Vec.fromList [0,1,2]) ([(0 0 0), (0 1 0)])
---
-mkRLGVTK :: (RenderElemVTK a)=> String -> Vector Double -> Vector Double -> Vector Double ->
-            [VTKAttrPoint a] -> VTK a
-mkRLGVTK name px py pz pointData = let
-  nameTxt = pack name
-  dataset = mkRectLinGrid px py pz
-  in VTK nameTxt dataset [] pointData []
+instance RenderVox EdgeVoxelPos where
+  type VTKElem EdgeVoxelPos = (Int, Int)
+  renderVox = renderEdgePos
 
--- | Creates an Structured Grid dataset. Use:
---
--- > mkSGVTK "name" 2 1 1 ([(0 0 0), (0 1 0)])
---
-mkSGVTK :: (RenderElemVTK a)=> String -> Int -> Int -> Int -> [VTKAttrPoint a] -> VTK a
-mkSGVTK name nx ny nz pointData = let
-  nameTxt = pack name
-  dataset = StructGrid { dimSG = (nx, ny, nz) }
-  in VTK nameTxt dataset [] pointData []
+instance RenderVox VoxelPos where
+  type VTKElem VoxelPos = Int
+  renderVox = renderVoxelPos
 
--- | Creates an Structured Points dataset (ImageData). Use:
---
--- > mkSGVTK "name" (2, 1, 1) (0.0, 0.0, 0.0) (5.0, 5.0, 5.0) ([(0 0 0), (0 1 0)])
---
-mkSPVTK :: (RenderElemVTK a)=> String -> (Int, Int, Int) -> (Double, Double, Double)
-        -> (Double, Double, Double) -> [VTKAttrPoint a] -> VTK a
-mkSPVTK name (dx, dy, dz) orig spc pointData = let
-  nameTxt = pack name
-  dataset = StructPoint { dimSP    = (dx-1, dy-1, dz-1)
-                        , originSP = orig
-                        , spaceSP  = spc }
-  in VTK nameTxt dataset [] pointData []
+renderFacePos :: VoxBoxExt a -> FaceVoxelPos -> (Int, Int, Int, Int)
+renderFacePos (VoxBoxExt vbox) face = let
+  e p  = case getVoxelID (dimension vbox) p of
+    Just x -> x
+    _      -> error ("[VTK.VoxBox] " ++ show face ++ show (dimension vbox))
+  f000 = e
+  f100 = e . (#+# VoxelPos 1 0 0)
+  f010 = e . (#+# VoxelPos 0 1 0)
+  f110 = e . (#+# VoxelPos 1 1 0)
+  f001 = e . (#+# VoxelPos 0 0 1)
+  f101 = e . (#+# VoxelPos 1 0 1)
+  f011 = e . (#+# VoxelPos 0 1 1)
+  in case face of
+    Fx p -> (f000 p, f010 p, f011 p, f001 p)
+    Fy p -> (f000 p, f100 p, f101 p, f001 p)
+    Fz p -> (f000 p, f100 p, f110 p, f010 p)
 
--- | Adds data to all points in 'VTK'. Internally, it pass the data as a function
--- 'VTKAttPoint'.
---
--- > let attr = mkPointsAttr "grainID" (\i x -> x * grainIDTable!i)
--- > addPointValueAttr vtk
---
-addPointValueAttr :: (RenderElemVTK a)=> VTK a -> VTKAttrPointValue a -> VTK a
-addPointValueAttr VTK{..} attr = VTK { pointValueData = attr:pointValueData, .. }
+renderEdgePos :: VoxBoxExt a -> EdgeVoxelPos -> (Int, Int)
+renderEdgePos (VoxBoxExt vbox) edge = let
+  e p  = case getVoxelID (dimension vbox) p of
+    Just x -> x
+    _      -> error ("[VTK.VoxBox] " ++ show edge ++ show (dimension vbox))
+  f000 = e
+  f100 = e . (#+# VoxelPos 1 0 0)
+  f010 = e . (#+# VoxelPos 0 1 0)
+  f001 = e . (#+# VoxelPos 0 0 1)
+  in case edge of
+    Ex p -> (f000 p, f100 p)
+    Ey p -> (f000 p, f010 p)
+    Ez p -> (f000 p, f001 p)
 
--- | Adds data to all points in 'VTK'. Internally, it pass the data as a function
--- 'VTKAttPoint'.
---
--- > let attr = mkPointsAttr "grainID" (grainIDTable !)
--- > addPointAttr vtk
---
-addPointAttr :: (RenderElemVTK a)=> VTK a -> VTKAttrPoint a -> VTK a
-addPointAttr VTK{..} attr = VTK { pointData = attr:pointData, .. }
+renderVoxelPos :: VoxBoxExt a -> VoxelPos -> Int
+renderVoxelPos (VoxBoxExt vbox) p = case getVoxelID (dimension vbox) p of
+  Just x -> x
+  _      -> error ("[VTK.VoxBox] " ++ show p ++ show (dimension vbox))
 
--- | Adds data to all points in 'VTK'. Internally, it pass the data as a function
--- 'VTKAttPoint'.
---
--- > let attr = mkCellAttr "color" (\i x cellType -> (Vec3 1 1 1) &* 1/(evalCellType cellType))
--- > addCellAttr vtk attr
---
-addCellAttr :: (RenderElemVTK a)=> VTK a -> VTKAttrCell a -> VTK a
-addCellAttr VTK{..} attr = VTK { cellData = attr:cellData, .. }
+-- | Extends the range ('VoxBoxDim') of an given 'VoxBox' in order to calculate all voxel
+-- corner points. See 'getVoxBoxCornersPoints'.
+getExtendedVoxBox :: VoxBox a -> VoxBoxExt a
+getExtendedVoxBox vbox = let
+  func (VoxBoxDim x y z) = VoxBoxDim (x+1) (y+1) (z+1)
+  range = let vbr = dimension vbox
+          in vbr { vbrDim = func $ vbrDim vbr}
+  in VoxBoxExt $ vbox { dimension = range }
 
--- ================================ Internal stuffs ======================================
+-- | Calculate all the voxel's corner points (vertices from voxel box) by calculating the
+-- base point of each 'VoxelPos' in a extended range.
+getVoxBoxCornersPoints :: VoxBoxExt a -> Vector Vec3
+getVoxBoxCornersPoints (VoxBoxExt vbox) = let
+  dimData = dimension vbox
+  origin  = vbrOrigin dimData
+  size    = sizeVoxBoxRange dimData
+  VoxBoxDim dbx dby _ = vbrDim dimData
+  foo i = let
+    (z, rz) = i  `quotRem` (dbx * dby)
+    (y, ry) = rz `quotRem` dbx
+    x       = ry
+    in evalVoxelPos vbox (origin #+# VoxelPos x y z)
+  in U.generate size foo
 
-type CellBuilder = ([Vector Int], [Int], [CellType], Int)
+-- =======================================================================================
 
-addCell :: (RenderCell shape)=> CellBuilder -> shape -> CellBuilder
-addCell set@(cellUG, cellOffUG, cellTypeUG, offCount) obj
-  | cell_size <= 0 = set
-  | otherwise = (cell : cellUG, cell_off : cellOffUG, cell_type : cellTypeUG , cell_off)
-  where
-    cell       = makeCell obj
-    cell_off   = offCount + cell_size
-    cell_size  = U.length cell
-    cell_type  = getType obj
+-- | Render to VTK using RectlinearGrid as points.
+-- A @VoxBoxDim@ of x, y and z will result in x*z*y points
+renderVoxBoxVTK :: VoxBox a -> [VTKAttrPoint Double] -> VTK Double
+renderVoxBoxVTK vbox attrs = let
+  (dx,dy,dz) = getVoxBoxDim . vbrDim $ dimension vbox
+  vx  = U.generate dx (evalLinPos vbox XDir)
+  vy  = U.generate dy (evalLinPos vbox YDir)
+  vz  = U.generate dz (evalLinPos vbox ZDir)
+  in mkRLGVTK "VoxBoxPoints" vx vy vz attrs
 
-mkUnstructGrid :: (RenderElemVTK p, RenderCell shape, FastFoldable cont shape)=>
-                  Vector p -> cont shape -> VTKDataSet p
-mkUnstructGrid points cells = let
-  i = ([], [], [], 0)
-  (cell, cell_off, cell_type, _) = folder addCell i cells
-  in UnstructGrid { setUG      = points
-                  , cellUG     = U.concat cell
-                  , cellOffUG  = U.reverse $ U.fromList cell_off
-                  , cellTypeUG = V.reverse $ V.fromList cell_type }
+renderVoxElemListVTK :: (RenderCell (VTKElem elem), RenderVox elem)=>
+                        VoxBox a -> [elem] -> VTK Vec3
+renderVoxElemListVTK vbox v = let
+  vbext = getExtendedVoxBox vbox
+  ps    = getVoxBoxCornersPoints vbext
+  color = U.generate (length v) id
+  rs    = map (renderVox vbext) v
+  attr  = mkCellAttr "color" (\a _ _ -> color U.! a)
+  in mkUGVTK "MicroGraph" ps rs [] [attr]
 
-mkRectLinGrid :: (RenderElemVTK a)=> Vector Double ->
-                 Vector Double ->  Vector Double -> VTKDataSet a
-mkRectLinGrid x y z = RectLinGrid { setxRG = x, setyRG = y, setzRG = z }
+renderVoxElemVTK :: (Unbox elem, RenderCell (VTKElem elem), RenderVox elem)=>
+                    VoxBox a -> [V.Vector elem] -> VTK Vec3
+renderVoxElemVTK vbox v = let
+  vbext = getExtendedVoxBox vbox
+  ps    = getVoxBoxCornersPoints vbext
+  color = U.concat $ map (\(fid, x) -> U.replicate (V.length x) fid) $ zip [(1::Int) ..] v
+  rs    = V.map (renderVox vbext) $ V.concat v
+  attr  = mkCellAttr "color" (\a _ _ -> color U.! a)
+  in mkUGVTK "MicroGraph" ps rs [] [attr]
 
--- ================================ Cell containers ======================================
+renderAllElemProp :: (RenderCell (VTKElem elem), RenderVox elem, HasPropValue prop)
+                  => VoxBox a -> [prop (V.Vector elem)] -> VTK Vec3
+renderAllElemProp vbox vec = let
+  ps = mapMaybe getPropValue vec
+  in renderVoxElemVTK vbox ps
 
--- | Special foldable class since `Foldable` doesn't support unboxed vectors
-class FastFoldable cont b where
-  folder :: (a -> b -> a) -> a -> cont b -> a
+renderMicroGrainsVTK :: VoxBox a -> MicroVoxel -> VTK Vec3
+renderMicroGrainsVTK vbox mv = renderAllElemProp vbox (HM.elems $ microGrains mv)
 
-instance FastFoldable [] a where
-  folder func = L.foldr (flip func)
+renderMicroEdgesVTK :: VoxBox a -> MicroVoxel -> VTK Vec3
+renderMicroEdgesVTK vbox mv = renderAllElemProp vbox (HM.elems $ microEdges mv)
 
-instance (U.Unbox a)=> FastFoldable Vector a where
-  folder func = U.foldr' (flip func)
+renderMicroFacesVTK :: VoxBox a -> MicroVoxel -> VTK Vec3
+renderMicroFacesVTK vbox mv = renderAllElemProp vbox (HM.elems $ microFaces mv)
 
-instance FastFoldable V.Vector a where
-  folder func = V.foldr' (flip func)
+renderMicroVertexVTK :: VoxBox a -> MicroVoxel -> VTK Vec3
+renderMicroVertexVTK vbox mv = let
+  ps = mapMaybe getPropValue (HM.elems $ microVertex mv)
+  in renderVoxElemListVTK vbox ps
 
-instance FastFoldable IntMap a where
-  folder func = IM.fold (flip func)
+-- =======================================================================================
 
--- ================================ Basic instances ======================================
+instance RenderCell (Int, Int, Int, Int) where
+  makeCell (a,b,c,d) = U.fromList [a,b,c,d]
+  getType _          = VTK_QUAD
 
-instance RenderCell (Int, Int, Int) where
-  makeCell (a,b,c) = U.fromList [a,b,c]
-  getType _        = VTK_TRIANGLE
+instance RenderElemVTK Normal3
+instance RenderElemVTK Vec3
+instance RenderElemVTK Vec2
+instance RenderElemVTK Mat3
 
-instance RenderCell (Int, Int) where
-  makeCell (a,b) = U.fromList [a,b]
-  getType _      = VTK_LINE
+instance RenderPoint Mat3 where
+  renderPoint (Mat3 x y z) =
+    renderPoint x <>
+    renderPoint y <>
+    renderPoint z
+  renderBinaryPoint (Mat3 x y z) =
+    renderBinaryPoint x <>
+    renderBinaryPoint y <>
+    renderBinaryPoint z
+  pointNumberType = const VTK_Float
+  pointNumberComp = const 9
 
-instance RenderCell Int where
-  makeCell a = U.singleton a
-  getType _  = VTK_VERTEX
+instance RenderPoint Vec3 where
+  renderPoint (Vec3 x y z) =
+    renderPoint x <>
+    renderPoint y <>
+    renderPoint z
+  renderBinaryPoint (Vec3 x y z) =
+    renderBinaryPoint x <>
+    renderBinaryPoint y <>
+    renderBinaryPoint z
+  pointNumberType = const VTK_Float
+  pointNumberComp = const 3
+
+instance RenderPoint Vec2 where
+  renderPoint (Vec2 x y) =
+    renderPoint x <>
+    renderPoint y <>
+    renderPoint (0 :: Double)
+  renderBinaryPoint (Vec2 x y) =
+    renderBinaryPoint x <>
+    renderBinaryPoint y <>
+    renderBinaryPoint (0 :: Double)
+  pointNumberType = const VTK_Float
+  pointNumberComp = const 3
+
+instance RenderPoint Normal3 where
+  renderPoint = renderPoint . fromNormal
+  renderBinaryPoint = renderBinaryPoint . fromNormal
+  pointNumberType = const VTK_Float
+  pointNumberComp = const 3
